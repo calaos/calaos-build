a6e46e78ed0ccac711854c19fa02523a26e64ef1
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2022-01-03 15:16:50 +0100

    Create local repo, only if packages are present in final directory

--- update_localrepo.sh
+++ update_localrepo.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-mkdir -p /src/out/pkgs/x86_64/
-repo-add /src/out/pkgs/x86_64/calaos.db.tar.gz /src/out/pkgs/x86_64/*.tar.zst
-
-echo -e\
+ls /src/out/pkgs/x86_64/*.tar.zst
+if [ $? -eq 0 ]; then
+    repo-add /src/out/pkgs/x86_64/calaos.db.tar.gz /src/out/pkgs/x86_64/*.tar.zst
+    echo -e\
     "[calaos]\n"\
     "SigLevel = Optional TrustAll\n"\
     "Server = file:///src/out/pkgs/x86_64\n"\
     >>  /etc/pacman.conf
+    pacman -Sy --noconfirm
+else
+     mkdir -p /src/out/pkgs/x86_64/
+fi
 
-pacman -Sy --noconfirm
+
