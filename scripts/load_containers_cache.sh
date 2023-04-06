4919d5e918b271a7e3fbf0ae7c274f534a2b51d4
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2023-04-06 23:25:46 +0200

    fix container caches

--- scripts/load_containers_cache.sh
+++ scripts/load_containers_cache.sh
@@ -3,7 +3,7 @@
 for image in /var/lib/cache/containers/* ; do
     name="${image##*/}"
     mkdir -p /src/out/containers/
-    skopeo copy oci:$image containers-storage://$name:latest
+    skopeo copy oci:$image containers-storage:$name:latest
 done
 
 rm -rf /var/lib/cache/containers
