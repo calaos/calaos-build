b3f12f0c539e8eec752d89f921427523e3d4207b
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2023-04-11 21:42:47 +0200

    load cache before running container

--- scripts/cache_images.sh
+++ scripts/cache_images.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 set -e
-
-SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source $SCRIPTDIR/calaos_lib.sh
-
-mkdir -p "${HOME}/calaos/calaos_docker"
-
+# iterate on all container files
 for f in /src/calaos-os/services/*.container ; do
     image=$(cat $f | grep Image= | cut -d= -f 2)
     filename=$(cat $f | grep ContainerName= | cut -d= -f 2)
-    path=$(echo $image | cut -d / -f 2- ) 
-    echo $path
-    
-
     mkdir -p /src/out/containers/
-    skopeo copy docker://$image oci:/src/out/containers/$filename
-    #sudo podman save -o /src/out/containers/$filename.tar --format oci-archive $image
+    if [ ! -d /src/out/containers/"$filename" ]
+    then
+        # Pull image
+        sudo podman pull "$image"
+        # Save image as oci-dir and compressed it
+        sudo podman save --compress --format oci-dir -o /src/out/containers/"$filename" "$image"
+    fi
 done
--- scripts/load_containers_cache.sh
+++ scripts/load_containers_cache.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-for image in /var/lib/cache/containers/* ; do
-    name="${image##*/}"
-    mkdir -p /src/out/containers/
-    skopeo copy oci:$image containers-storage:$name:latest
-done
+# build container cache directory
+cache=/var/lib/cache/containers/$1
 
-rm -rf /var/lib/cache/containers
+if [ -d "$cache" ]
+then
+    echo "loading $cache"
+    # Podman loads directory as cache and put it in localhost/... repository
+    podman load -i "$cache"
+    # Retry image name with repositiory name (docker.io|ghcr.io|...)
+    image=$(cat  /etc/containers/systemd/$1.container | grep Image= | cut -d= -f 2)
+    echo "Image : $image"
+    # Get SHA of image loaded
+    sha=$(podman images -a | grep $1 | awk -F ' ' '{ print $3 }')
+    echo "SHA : $sha"
+    # tag it SHA with the full qualified image name to be used in the container
+    podman image tag "$sha" "$image"
+    # Remove the cache
+    rm -rf "$cache"
+fi
